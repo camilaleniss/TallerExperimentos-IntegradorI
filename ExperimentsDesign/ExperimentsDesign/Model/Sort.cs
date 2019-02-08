ebc3d219d611d0beef19043e6c429a4b1463bf76
camilaleniss <mlmariacami2@gmail.com>
2019-02-08 17:08:15 -0500

    Creado proyecto y clase Sorter
    Falta definir bien lo genérico

---
+++ ExperimentsDesign/ExperimentsDesign/Model/Sort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExperimentsDesign.Model
+{
+    public interface ISort<T>
+    {
+        T[] SelectionSort(T[] array);
+        T[] InsertionSort(T[] array);
+
+    }
+}
---
+++ ExperimentsDesign/ExperimentsDesign/Model/Sorter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExperimentsDesign.Model
+{
+    class Sorter<T> : ISort<T>
+    {
+        public Sorter()
+        {
+        }
+
+        public T[] InsertionSort(T[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                //Corregir comparación
+                for (int j=i; j>0 && array[j-1]>array[j]; j--)
+                {
+                    T temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+            return array;
+        }
+
+        public T[] SelectionSort(T[] array)
+        {
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                T menor = array[i];
+                int which = i;
+                for (int j =i+1; j < array.Length; j++)
+                {
+                    //Corregir comparación
+                    if (array[j] < menor)
+                    {
+                        menor = array[j];
+                        which = j;
+                    }
+                }
+                T temp = array[i];
+                array[i] = menor;
+                array[which] = temp;
+            }
+            return array;
+        }
+        
+    }
+}
