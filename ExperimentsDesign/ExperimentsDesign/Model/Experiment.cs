2aad29c2a290add8a257a103c242fb3ccd4b7ff3
camilaleniss <mlmariacami2@gmail.com>
2019-02-09 18:54:10 -0500

    Ensayo esqueleto app
    Añadidas clases Experiment y Test

---
+++ ExperimentsDesign/ExperimentsDesign/Model/Experiment.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExperimentsDesign.Model
+{
+    public class Experiment
+    {
+        private List<Test<T>> tests { get; set; }
+
+        public Experiment()
+        {
+
+        }
+
+        public void ExecuteExperiment()
+        {
+
+        }
+
+        public void InitTests()
+        {
+            InitMatrixTests();
+            CreateTests();
+        }
+
+        public void InitMatrixTests()
+        {
+
+        }
+
+        public void CreateTests()
+        {
+
+        }
+
+        public void ExecuteTest(Test<T> test)
+        {
+
+            test.ExecuteTest();
+        }
+
+        public void WriteCVS()
+        {
+
+        }
+
+
+
+
+        
+    }
+}
---
+++ ExperimentsDesign/ExperimentsDesign/Model/Test.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExperimentsDesign.Model
+{
+    public const int LEN1 = 10;
+    public const int LEN2 = 100;
+    public const int LEN3 = 1000;
+
+    public class Test<T>
+    {
+        private int Algorithm { get; set; }
+        private int Datatype {get; set; }
+        private int State {get; set; }
+        private int Length { get; set; }
+        private T[] Array { get; set; }
+        private long Time { get; }
+
+
+        public Test(int [] values)
+        {
+            Algorithm = values[0];
+            Datatype = values[1];
+            State = values[2];
+            Length = values[3];
+        }
+
+        public void initArray()
+        {
+            
+        }
+
+        public long ExecuteTest()
+        {
+            return 10;
+        }
+
+        private T[] sort (T[] array)
+        {
+            return new T[2];
+        }
+        private T[] SelectionSort(T[] array)
+        {
+            return new T[2];
+        }
+
+        private T[] InsertionSort(T[] array)
+        {
+            return new T[2];
+        }
+
+    }
+}
