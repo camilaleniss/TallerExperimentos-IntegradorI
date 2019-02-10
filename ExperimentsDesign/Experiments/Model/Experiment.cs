cdc97d16a856f9eca8c6f5519999ac40f5962afa
Javier Torres <javierandrestorresreyes@gmail.com>
2019-02-10 12:34:16 -0500

    Eliminado codigo antiguo

--- ExperimentsDesign/Experiments/Model/Experiment.cs
+++ ExperimentsDesign/Experiments/Model/Experiment.cs
@@ -45,6 +45,10 @@ namespace Experiments.Model
             return treatment;
         }
 
+        /// <summary>
+        /// Initializes the treatements matrix as shown in the report of Experiments Design 
+        /// </summary>
+        /// <returns></returns>
         public int[,] InitMatrixTests()
         {
             int rows = 1;
@@ -73,66 +77,6 @@ namespace Experiments.Model
             }
             return matrix;
         }
-
-        /// <summary>
-        /// Initializes the treatements matrix as shown in the report of Experiments Design 
-        /// </summary>
-        /// <returns></returns>
-        //public int[,] InitMatrixTests()
-        //{
-        //    int[,] matrix = new int[72, 4];
-        //    //First column
-        //    for (int i = 0; i < matrix.GetLength(0); i++)
-        //    {
-        //        matrix[i, 0] = (i < 36) ? 1 : 2;
-        //    }
-        //    //Second column
-        //    int num = 1;
-        //    int row = 0;
-        //    for (int i = 0; i < 2; i++)
-        //    {
-        //        num = 1;
-        //        for (int j = 0; j < 4; j++)
-        //        {
-        //            for (int k = 0; k < 9; k++)
-        //            {
-        //                matrix[row, 1] = num;
-        //                row++;
-        //            }
-        //            num++;
-        //        }
-        //    }
-        //    //Third column
-        //    num = 1;
-        //    row = 0;
-        //    for (int i = 0; i < 8; i++)
-        //    {
-        //        num = 1;
-        //        for (int j = 0; j < 3; j++)
-        //        {
-        //            for (int k = 0; k < 3; k++)
-        //            {
-        //                matrix[row, 2] = num;
-        //                row++;
-        //            }
-        //            num++;
-        //        }
-        //    }
-        //    //Fourth column
-        //    num = 1;
-        //    row = 0;
-        //    for (int i = 0; i < 24; i++)
-        //    {
-        //        num = 1;
-        //        for (int j = 0; j < 3; j++)
-        //        {
-        //            matrix[row, 3] = num;
-        //            row++;
-        //            num++;
-        //        }
-        //    }
-        //    return matrix;
-        //}
 
         /// <summary>
         /// Creates 72 Treatments where the values of each treatments are a row of the matrix
