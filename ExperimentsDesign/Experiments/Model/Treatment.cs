cd1fb1e4b874425bd8e7d8dea7cc59ef127f6a97
Javier Torres <javierandrestorresreyes@gmail.com>
2019-02-10 13:50:22 -0500

    Lista generación aleatoria

--- ExperimentsDesign/Experiments/Model/Treatment.cs
+++ ExperimentsDesign/Experiments/Model/Treatment.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -13,13 +14,18 @@ namespace Experiments.Model
         public const int INT_32 = 1;
         public const int STRING = 2;
         public const int DOUBLE = 3;
-        public const int OBJECT = 4;
+        public const int INT_64 = 4;
 
         public const int NOT_ORDERED = 1;
         public const int ASCENDING_ORDER = 2;
         public const int DESCENDING_ORDER = 3;
 
-        public static readonly int[] SIZES = new int[] { 100, 1000, 10000 };
+        public const int MIN_INT = -10000;
+        public const int MAX_INT = 10000;
+
+        public const int MAX_STRING_SIZE = 1000;
+
+        public static readonly int[] SIZES = new int[] {0, 100, 1000, 10000 };
 
         private int algorithm;
         private int datatype;
@@ -27,7 +33,7 @@ namespace Experiments.Model
         private int length;
         private long time;
 
-        private Boolean isDone { get; set; }
+        private IList array;
 
 
         public Treatment(int [] values)
@@ -36,12 +42,69 @@ namespace Experiments.Model
             datatype = values[1];
             state = values[2];
             length = values[3];
-            isDone = false;
+
+            InitArray();
+            FillArray(new Random());
         }
 
-        public void initArray()
+        private void InitArray()
         {
-            
+            switch(datatype)
+            {
+                case INT_32:
+                    array = new int[SIZES[length]];
+                    break;
+
+                case STRING:
+                    array = new string[SIZES[length]];
+                    break;
+
+                case DOUBLE:
+                    array = new double[SIZES[length]];
+                    break;
+
+                case INT_64:
+                    array = new long[SIZES[length]];
+                    break;
+            }            
+        }
+
+        private void FillArray(Random random)
+        {
+            for (int i = 0; i < array.Count; i++)
+            {
+                switch (datatype)
+                {
+                    case INT_32:
+                        array[i] = random.Next(MIN_INT, MAX_INT);
+                        break;
+
+                    case STRING:
+                        array[i] = GetRandomString(random);
+                        break;
+
+                    case DOUBLE:
+                        array[i] = random.NextDouble() * (MAX_INT - MIN_INT) + MIN_INT;
+                        break;
+
+                    case INT_64:
+                        array[i] = (long)random.Next(MIN_INT, MAX_INT);
+                        break;
+                }
+            }
+        }
+
+        private string GetRandomString(Random random)
+        {
+            char[] chars = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&".ToCharArray();
+            int size = random.Next(1, MAX_STRING_SIZE);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < size; i++)
+            {
+                int index = random.Next(chars.Length);
+                sb.Append(chars[index]);
+            }
+            return sb.ToString();
         }
 
         public long ExecuteTest()
