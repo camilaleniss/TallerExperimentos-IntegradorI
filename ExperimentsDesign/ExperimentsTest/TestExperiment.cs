d1fbc729ba275e5d158b666ddab9a52e445ef630
camilaleniss <mlmariacami2@gmail.com>
2019-02-10 14:34:45 -0500

    Added escenarios y prueba initMatrixTests

--- ExperimentsDesign/ExperimentsTest/TestExperiment.cs
+++ ExperimentsDesign/ExperimentsTest/TestExperiment.cs
@@ -5,19 +5,85 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ExperimentsTest
 {
     [TestClass]
-    public class UnitTest1
+    public class TestExperiment
     {
-        private Experiment exp;
+        private Experiment experiment;
 
-        private void SetupScenario1()
+        public TestExperiment()
         {
-            exp = new Experiment();
+            experiment = new Experiment();
         }
 
-        [TestMethod]
-        public void TestGetNextTreatment()
+        public void SetUpStage1()
         {
-            SetupScenario1();
+            
+        }
+    
+        [TestMethod]
+        public void TestInitMatrixTests()
+        {
+            int[,] matrix = experiment.InitMatrixTests();
+
+            //First column
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (i < 36)
+                {
+                    Assert.AreEqual(matrix[i, 0] , 1);
+                }
+                else
+                {
+                    Assert.AreEqual(matrix[i, 0] , 2);
+                }
+            }
+
+            //Second column
+            int num = 1;
+            int row = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                num = 1;
+                for (int j = 0; j < 4; j++)
+                {
+                    for (int k = 0; k < 9; k++)
+                    {
+                        Assert.AreEqual(matrix[row, 1] , num);
+                        row++;
+                    }
+                    num++;
+                }
+            }
+
+            //Third column
+            num = 1;
+            row = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                num = 1;
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        Assert.AreEqual(matrix[row, 2] , num);
+                        row++;
+                    }
+                    num++;
+                }
+            }
+
+            //Fourth column
+            num = 1;
+            row = 0;
+            for (int i = 0; i < 24; i++)
+            {
+                num = 1;
+                for (int j = 0; j < 3; j++)
+                {
+                    Assert.AreEqual(matrix[row, 3] , num);
+                    row++;
+                    num++;
+                }
+            }
         }
     }
 }
---
+++ ExperimentsDesign/ExperimentsTest/TestTreatment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExperimentsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private Treatment treat;
+
+        public void SetUpStage1()
+        {
+            treat = new Treatment(new int[1, 1, 1, 1]);
+        }
+
+        public void SetUpStage2()
+        {
+            treat = new Treatment(new int[1, 2, 1, 1]);
+        }
+
+        public void SetUpStage3()
+        {
+            treat = new Treatment(new int[1, 3, 2, 2]);
+        }
+
+        public void SetUpStage4()
+        {
+            treat = new Treatment(new int[1, 3, 3, 3]);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
